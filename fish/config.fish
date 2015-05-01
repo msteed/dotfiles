670cf8156bc0ef738c9f6c6e983b9bcc3f4dc3e1
Michael Steed <msteed@saltstack.com>
2015-05-01 09:15:57 -0600

    fish and vim

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,40 @@
+function dropbox
+    dropbox-cli "$argv"
+end
+
 function global
     /usr/bin/global --result=grep "$argv"
+end
+
+function grep
+    /usr/bin/grep --color=always "$argv"
+end
+
+function ll
+    /usr/bin/ls --color=always "$argv"
+end
+
+function man
+    # LESS_TERMCAP_mb - begin blink
+    # LESS_TERMCAP_md - begin bold
+    # LESS_TERMCAP_me - end mode
+    # LESS_TERMCAP_so - begin standout
+    # LESS_TERMCAP_se - end standout
+    # LESS_TERMCAP_us - begin underline
+    # LESS_TERMCAP_ue - end underline
+    env \
+        LESS_TERMCAP_mb=(set_color blue) \
+        LESS_TERMCAP_md=(set_color -o blue) \
+        LESS_TERMCAP_me=(set_color normal) \
+        LESS_TERMCAP_so=(set_color yellow) \
+        LESS_TERMCAP_se=(set_color normal) \
+        LESS_TERMCAP_us=(set_color magenta) \
+        LESS_TERMCAP_ue=(set_color normal) \
+        man "$argv"
+end
+
+function pylint
+    pylint2 --rcfile=$HOME/salt/.testing.pylintrc -r no $argv
 end
 
 set PATH $HOME/bin /usr/local/bin /usr/bin
