059f0dfc3f234698c9f2eaa823f718934586f9f1
Michael Steed <msteed@saltstack.com>
2015-06-24 14:37:20 -0600

    pylint & pylint2

--- fish/functions/pylint2.fish
+++ fish/functions/pylint2.fish
@@ -1,4 +1,4 @@
-function pylint
+function pylint2
     set rcfile .testing.pylintrc
     pushd .
     while [ (pwd) != $HOME -a (pwd) != / -a ! -e ./$rcfile ]
@@ -7,7 +7,7 @@ function pylint
     if [ -e ./$rcfile ]
         set rcfile (readlink -e ./$rcfile)
         popd
-        /usr/bin/pylint --rcfile=$rcfile -r no $argv
+        /usr/bin/pylint2 --rcfile=$rcfile -r no $argv
     else
         echo "no .testing.pylintrc found"
         return 1
