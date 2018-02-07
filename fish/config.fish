7d5e2c5118d56d0c37a1a5db65872ae3c3705d9f
Michael Steed <msteed68@gmail.com>
2018-02-07 16:57:23 -0700

    fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,11 @@
-set PATH $HOME/bin /usr/local/bin /usr/bin
+set PATH $HOME/bin /usr/bin
 
 set -x EDITOR "nvim"
 set -x PAGER "less"
 set -x LESS "-I -R -S --shift=8"
 #set -x SHELL "/usr/bin/fish"
 set -x BROWSER "chromium"
-set -x PYTHONPATH "$HOME/salt-current:$HOME/raas-current:$HOME/salt-pylint:$HOME/salt-testing"
+set -x PYTHONPATH "$HOME/raas-current"
 
 set fish_greeting ""
 
@@ -17,9 +17,7 @@ set -g fish_key_bindings my_vi_key_bindings
 #set fish_cursor_visual '#8c9440'   # green
 #my_fish_vi_cursor
 
-abbr -a nv nvim
 abbr -a mtp mktags-python
-abbr -a ts timer -s
 abbr -a hd hexdump -C
 
 abbr -a gs git status
@@ -29,4 +27,6 @@ abbr -a ga git add
 abbr -a gb git branch
 abbr -a gci git commit
 abbr -a gco git checkout
+abbr -a gf git fetch
+abbr -a gp git pull
 abbr -a gw git worktree
